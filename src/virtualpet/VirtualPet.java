cdafe636da91a9421641412e93ee98bed1083a6e
DavidShen1 <119268479+DavidShen1@users.noreply.github.com>
2024-03-20 09:06:25 -0400

    Part 1

--- src/virtualpet/VirtualPet.java
+++ src/virtualpet/VirtualPet.java
@@ -15,6 +15,21 @@ public class VirtualPet {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("            |\\_/|        D\\___/\\\n" +
+                            "            (0_0)         (0_o)\n" +
+                            "           ==(Y)==         (V)\n" +
+                            "----------(u)---(u)----oOo--U--oOo---\n" +
+                            "__|_______|_______|_______|_______|___");
+        
+        System.out.println("               PET ISLAND");
+        
+        System.out.println("\n1. Start\n2. Instructions\n3. Exit");
+        System.out.print("Please make a selection(1,2 or 3): ");
+        int selection = input.nextInt();
+        
+
     }
     
 }
